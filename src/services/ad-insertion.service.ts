aa4bf6222bd73b7ece083fd7bbdb6aa4807c7eff
Kundan Kumar <c-kundan.kumar@timesgroup.com>
2024-08-20 04:33:00 +0530

    project-setup-done

--- src/services/ad-insertion.service.ts
+++ src/services/ad-insertion.service.ts
@@ -9,34 +9,17 @@ export class AdInsertionService {
   /*
    * Add service methods here
    */
-  // async insertAds(venues: any, zone: string) {
-  //   const ads = await this.venueAdvertiseRepository.find({where: {zone: zone}});
-
-  //   // Insert ads at predefined positions
-  //   ads.forEach(ad => {
-  //     if (ad.insertionPosition < venues.length) {
-  //       venues.splice(ad.insertionPosition, 0, ad);
-  //     } else {
-  //       venues.push(ad);
-  //     }
-  //   });
-
-  //   return venues;
-  // }
   async insertAds(venues: any[], zone: string) {
     // Fetch ads for the given zone
     const ads = await this.venueAdvertiseRepository.find({ where: { zone: zone } });
 
-    // Create a mapping of venueId to insertionPosition
     const adMap = new Map<string, number>();
     ads.forEach(ad => {
       adMap.set(ad.venueId.toString(), ad.insertionPosition);
     });
 
-    // Create an array to hold reordered venues
     const orderedVenues = new Array(venues.length).fill(null);
 
-    // Place ads at their predefined positions
     adMap.forEach((insertionPosition, venueId) => {
       const venue = venues.find(v => v._id.toString() === venueId);
       if (venue) {
@@ -47,11 +30,9 @@ export class AdInsertionService {
       }
     });
 
-    // Place remaining venues in the gaps
     let currentPosition = 0;
     venues.forEach(venue => {
       if (!adMap.has(venue._id.toString())) {
-        // Find the next available position
         while (orderedVenues[currentPosition] !== null) {
           currentPosition++;
         }
